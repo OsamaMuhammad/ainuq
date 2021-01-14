7f0e8ecc8a7bd4e94ae926658deb1c57f931a572
osama <osama.muhammad@koderlabs.com>
2021-01-14 18:39:59 +0500

    Initial commit

---
+++ app/src/main/java/com/app/ainuq/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.app.ainuq
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.squareup.moshi.Moshi
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
+
+fun main() {
+
+}
